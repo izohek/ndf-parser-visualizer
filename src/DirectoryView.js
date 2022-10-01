734c2f6b13a2950525082043905bfb269b3c8f5e
izohek <izohek@gmail.com>
2022-10-01 12:36:40 -0700

    Added Bessieres ndf files and a mechanism to switch between the files for different patches.

--- src/DirectoryView.js
+++ src/DirectoryView.js
@@ -1,20 +1,41 @@
-import React from "react"
-import { filesMassena } from "./files/FileDir";
+import React, { useState } from "react"
+import { files } from "./files/FileDir";
+
+const patches = [
+    "massena",
+    "bessieres"
+];
 
 export default function DirectoryView({root, selected}) {
 
+    const [activePatch, setActivePatch] = useState('bessieres')
+
     return (
-        <div className="dir-view">
-            Files
-            <ul>
-            {
-                filesMassena.map((file) => {
-                    return (
-                        <li key={file} onClick={ e => selected(root + file)}>{file}</li>
-                    );
-                })
-            }
-            </ul>
+        <div>
+            <h3>Files</h3>
+            <div style={{textAlign:"left"}}>
+                <label htmlFor="patch-select" style={{marginRight:"1em"}}>Patch</label>
+                <select id="patch-select" name="patch-select" value={activePatch} onChange={(event) => { setActivePatch(event.target.value)} }>
+                    {
+                        patches.map((patch) => {
+                            return (
+                                <option value={patch} key={patch}>{patch}</option>
+                            )
+                        })
+                    }
+                </select>
+            </div>
+            <div className="dir-view">
+                <ul>
+                {
+                    files[activePatch].map((file) => {
+                        return (
+                            <li key={file} onClick={ e => selected(root + file)}>{file}</li>
+                        );
+                    })
+                }
+                </ul>
+            </div>
         </div>
     )
 }
